42d66fe6a7ac766961b6fd7269f6f1cc770837cb
Steve Villella <s.villella@live.com>
2017-01-17 19:40:57 -0500

    update exercise 7

--- exercise7.rb
+++ exercise7.rb
@@ -3,3 +3,12 @@ students = {
   :cohort2 => 42,
   :cohort3 => 22
 }
+
+
+students.each do |k, v|
+ puts "The cohort is #{k}: #{v} Students"
+ end
+
+# output.each do |key, value|
+#     puts key + ' : ' + value
+# end
